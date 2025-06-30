b241f8d9c898d2bd2b417f6530eef8f36566f53b
vicky b <bemiyi5428@nab4.com>
2025-06-30 12:26:24 +0000

    jinaja demo

---
+++ analyses/jinja_demos.sql
@@ -0,0 +1,45 @@
+{{ 'Welcome to dbt cloud' }}
+
+{# comment #}
+
+{# 
+multi
+line
+comment
+#}
+
+{% set var1 = 'Hello World without -' %}
+{{ var1 }}
+{%set var2 = 'Hello World with ' -%}
+{{ var2 }}
+
+{% set var3 = ['Hello World', 'welcome to dbt, World!'] -%}
+{{ var3 }}
+{{ var3[1] }}
+
+ 
+
+{# ------if else end if conditon------  #}
+{%- set temperature = 30 -%}
+
+{%- if temperature > -10 -%}
+{{ 'It is hot' }}
+{%- elif temperature < 0 -%}
+{{ 'It is freezing' }}
+{%- else -%}
+{{ 'It is not hot' }}
+{%- endif -%}
+
+
+
+{# ------for loop-----  #}
+{%- set col_lst = ['id', 'quantity', 'unit_price'] -%}
+
+SELECT 
+{%- for col in colitem_lst %}
+  {{ col }}
+  {%- if not loop.last -%}
+   ,
+  {%- endif -%}
+{%- endfor %}
+FROM table
